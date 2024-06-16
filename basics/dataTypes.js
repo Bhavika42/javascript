a112cd8527e7984d5e28dd5183d79a3c4143bc40
Bhavika42 <133744950+Bhavika42@users.noreply.github.com>
2024-06-16 07:48:48 +0000

    conv.

---
+++ basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we r using nodejs, not browser 
+
+console.log(3+3); 
+console.log("bhavika");
+
+let name = "Bhavika"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof age);
